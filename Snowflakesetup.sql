59689b2d5ac51f4ccac2b89b8fa699d0b8e2da59
EMMANUEL <50409804+emmy-1@users.noreply.github.com>
2025-01-29 23:37:02 +0000

    setup snowflake

---
+++ Snowflakesetup.sql
@@ -0,0 +1,48 @@
+USE ROLE ACCOUNTADMIN;
+
+CREATE OR REPLACE DATABASE Bitcoin_Transcations;
+
+GRANT USAGE ON WAREHOUSE bitcon_compute TO ROLE ACCOUNTADMIN;
+GRANT ALL ON DATABASE Bitcoin_Transcations TO ROLE ACCOUNTADMIN;
+GRANT ROLE ACCOUNTADMIN TO USER DIGIT;
+
+
+USE SCHEMA bitcoin_transcations.public;
+
+CREATE OR REPLACE TABLE unconfirmed_transactions (
+    SRC VARIANT
+);
+CREATE OR REPLACE WAREHOUSE bitcon_compute WITH
+    WAREHOUSE_SIZE = 'X-SMALL'
+    AUTO_SUSPEND = 100
+    AUTO_RESUME = TRUE
+    INITIALLY_SUSPENDED=TRUE;
+
+USE WAREHOUSE bitcon_compute;
+
+CREATE STORAGE INTEGRATION snowflake
+  TYPE = EXTERNAL_STAGE
+  STORAGE_PROVIDER = 'S3'
+  ENABLED = TRUE
+  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::043309364582:role/snowflake'
+  STORAGE_ALLOWED_LOCATIONS = ('s3://dataclutster');
+
+DESC INTEGRATION snowflake;
+
+CREATE OR REPLACE STAGE recent_transactions
+    url = 's3://dataclutster/transactions.json'
+    STORAGE_INTEGRATION = snowflake;
+
+LIST @recent_transactions;
+
+COPY INTO unconfirmed_transactions
+    FROM @recent_transactions
+    FILE_FORMAT = (TYPE = JSON);
+
+select * from unconfirmed_transactions;
+
+SELECT
+    SRC
+    FROM unconfirmed_transactions;
+
+
